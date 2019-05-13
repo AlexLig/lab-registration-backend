e8872ba00c07f7d9b74e948ecdb09f9679a0362a
AlexLig <ligkalex@gmail.com>
2019-05-13 18:00:15 +0300

    lab-class initial model

---
+++ src/lab-class/model.ts
@@ -0,0 +1,43 @@
+import mongoose from 'mongoose';
+import Joi from 'joi';
+const labClassSchema = new mongoose.Schema({
+  course: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  dayISO: {
+    type: Number,
+    required: true,
+    trim: true,
+  },
+  startHour: {
+    type: Date,
+    required: true,
+    trim: true,
+  },
+  finishHour: {
+    type: Date,
+    trim: true,
+    required: true,
+  },
+});
+
+const validationSchema = {
+  courseID: Joi.string()
+    .required()
+    .min(2)
+    .max(255),
+
+  dayISO: Joi.number()
+    .required()
+    .min(1)
+    .max(7),
+
+  startHour: Joi.date().required(),
+
+  finishHour: Joi.date().required(),
+};
+
+export const LabClass = mongoose.model('LabClass', labClassSchema);
+export const validateLabClass = (lab: any) => Joi.validate(lab, validationSchema);
