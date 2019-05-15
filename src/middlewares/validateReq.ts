f9fb06f949bd30fcec7b14999d1288d8851d1e1d
AlexLig <ligkalex@gmail.com>
2019-05-16 00:24:20 +0300

    Create validateReq.ts

---
+++ src/middlewares/validateReq.ts
@@ -0,0 +1,21 @@
+import { NextFunction, Request, Response } from 'express';
+import { plainToClass } from 'class-transformer';
+import { validate, ValidatorOptions } from 'class-validator';
+import { HttpError } from '../utils/HttpError';
+
+export const validateReq = (dto: any, validatorOptions?: ValidatorOptions) => async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const dtoInstance = plainToClass(dto, req.body);
+  const errors = await validate(dtoInstance, {
+    whitelist: true,
+    forbidUnknownValues: true,
+    ...validatorOptions,
+  });
+  if (errors.length > 0) return next(new HttpError(400, `Bad Request.\n${errors}`));
+
+  req.body = dtoInstance;
+  next();
+};
