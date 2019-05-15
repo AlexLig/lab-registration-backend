688d23cb6eebfc3e1e61119e26daefd11412aade
AlexLig <ligkalex@gmail.com>
2019-05-16 02:38:02 +0300

    fix path type

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ const app = express();
 
 // Middlewares
 app.use(express.json());
-app.use('/api/cources', courseRouter);
+app.use('/api/courses', courseRouter);
 app.use(errorHandler);
 
 // Home route
