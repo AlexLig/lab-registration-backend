dd26c1ab6da827d9502ad0e5d7141e25bb0b8ac0
AlexLig <ligkalex@gmail.com>
2019-05-16 00:24:13 +0300

    Create services.ts

---
+++ src/student/services.ts
@@ -0,0 +1,15 @@
+import { getRepository } from 'typeorm';
+import { Student } from './entity';
+import { StudentDto } from './dto';
+import { HttpError } from '../utils/HttpError';
+
+const getStudentRepository = () => getRepository(Student);
+
+export async function createStudent(dto: StudentDto) {
+  const repo = getStudentRepository();
+
+  const existingStudent = await repo.findOne({ where: { am: dto.am } });
+  if (existingStudent) throw new HttpError(400, 'Student already registered');
+
+  return repo.save(dto);
+}
