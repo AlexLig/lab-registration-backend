64d18cadf9c54907d9504349255bce2e2d538bc1
AlexLig <ligkalex@gmail.com>
2019-05-16 15:24:09 +0300

    update studentRouter

--- src/student/router.ts
+++ src/student/router.ts
@@ -1,17 +1,26 @@
 import express, { Request, Response, NextFunction } from 'express';
-import bcrypt from 'bcrypt';
-import { createStudent } from './services';
+import { createStudent, getAllStudents } from './services';
 import { trim } from '../middlewares/trim';
 import { validateReq } from '../middlewares/validateReq';
 import { StudentDto } from './dto';
 
-export const router = express.Router();
+export const studentRouter = express.Router();
 
-router.route('/').post(trim('body'), validateReq(StudentDto), async (req, res, next) => {
-  try {
-    const student = await createStudent(req.body);
-    return res.send(student);
-  } catch (error) {
-    next(error);
-  }
-});
+studentRouter
+  .route('/')
+  .post(trim('body'), validateReq(StudentDto), async (req, res, next) => {
+    try {
+      const student = await createStudent(req.body);
+      return res.send(student);
+    } catch (error) {
+      next(error);
+    }
+  })
+  .get(async (req, res, next) => {
+    try {
+      const students = await getAllStudents();
+      res.send(students);
+    } catch (error) {
+      next(error);
+    }
+  });
