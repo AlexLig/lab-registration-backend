437edd707b3d5fc59378631e80da35c191cd5c35
AlexLig <ligkalex@gmail.com>
2019-05-17 16:11:41 +0300

    Fix typo. Hash password before saving it to db.

--- src/user/services.ts
+++ src/user/services.ts
@@ -3,14 +3,19 @@ import { getRepository } from 'typeorm';
 import { UserDto } from './dto';
 import { HttpError } from '../utils/HttpError';
 import { User } from './entity';
+import bcrypt from 'bcrypt';
 
 const getUserRepository = () => getRepository(User);
 
 export async function createUser(dto: UserDto) {
   const repo = getUserRepository();
 
-  const existingStudent = await repo.findOne({ where: { email: dto.email } });
-  if (existingStudent) throw new HttpError(400, 'User already registered');
+  const existingUser = await repo.findOne({ where: { email: dto.email } });
+  if (existingUser) throw new HttpError(400, 'User already registered');
 
-  return repo.save(dto);
+  const salt = await bcrypt.genSalt(10);
+  const password = await bcrypt.hash(dto.password, salt);
+  const user = { email: dto.email, password };
+
+  return repo.save(user);
 }
