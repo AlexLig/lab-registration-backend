8cee2dfc115d0e38c03ab8f768a2bbb30cfcc447
AlexLig <ligkalex@gmail.com>
2019-05-30 00:19:13 +0300

    Update register.ts

--- src/routers/register.ts
+++ src/routers/register.ts
@@ -1,11 +1,9 @@
-import express, { Request, Response, NextFunction } from 'express';
+import express from 'express';
 import { validateReq } from '../middlewares/validateReq';
 import { trim } from '../middlewares/trim';
 import { StudentDto } from '../student/dto';
-import { getRepository } from 'typeorm';
-import { User } from '../user/entity';
-import { Student } from '../student/entity';
 import { createStudentUser } from '../services/createStudentUser';
+import { removeProps } from '../utils/objectManipulation';
 
 export const registerRouter = express.Router();
 
@@ -13,11 +11,11 @@ registerRouter
   .route('/student')
   .post(trim('body'), validateReq(StudentDto), async (req, res, next) => {
     try {
-      const student = await createStudentUser(req.body);
-      return res.send(student);
+      const studentUser = await createStudentUser(req.body);
+      return res.send(removeProps(studentUser, 'password'));
     } catch (error) {
       next(error);
     }
   });
 
-registerRouter.route('/teacher').post(async (req, res, next) => {});
+registerRouter.route('/teacher').post(async () => {});
