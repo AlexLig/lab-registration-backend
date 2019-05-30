2c596723fb9acaee91c242a8b201d80bcdcb3f37
AlexLig <ligkalex@gmail.com>
2019-05-30 22:59:54 +0300

    res header with jwt .

--- src/routers/register.ts
+++ src/routers/register.ts
@@ -4,6 +4,7 @@ import { trim } from '../middlewares/trim';
 import { StudentDto } from '../student/dto';
 import { createStudentUser } from '../services/createStudentUser';
 import { removeProps } from '../utils/objectManipulation';
+import { generateAuthToken } from '../auth/generateToken';
 
 export const registerRouter = express.Router();
 
@@ -12,7 +13,13 @@ registerRouter
   .post(trim('body'), validateReq(StudentDto), async (req, res, next) => {
     try {
       const studentUser = await createStudentUser(req.body);
-      return res.send(removeProps(studentUser, 'password'));
+      const token = generateAuthToken({
+        id: studentUser.id,
+        isAdmin: studentUser.isAdmin,
+      });
+      return res
+        .header('x-auth-token', token)
+        .send(removeProps(studentUser, 'password'));
     } catch (error) {
       next(error);
     }
