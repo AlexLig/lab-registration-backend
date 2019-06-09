4b496b0ff77f0777491c8bf0fcefe3e7e88e0dc5
AlexLig <ligkalex@gmail.com>
2019-06-09 21:30:19 +0300

    Update getCourseLabs.ts

--- src/services/getCourseLabs.ts
+++ src/services/getCourseLabs.ts
@@ -1,8 +1,10 @@
 import { getRepository } from 'typeorm';
 import { Course } from '../course/entity';
+import { HttpError } from '../utils/HttpError';
 
 export async function getCourseLabs(id: number) {
   const courseRepo = getRepository(Course);
   const course = await courseRepo.findOne(id, { relations: ['labClasses'] });
-  if (course) return course.labClasses;
+  if (!course) throw new HttpError(404, 'Course with the given id does not exist');
+  return course.labClasses;
 }
