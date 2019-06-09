6cf1c20bad4122229f17812d89171ed53716de2f
AlexLig <ligkalex@gmail.com>
2019-06-09 21:19:14 +0300

    Update registerStudentToLab.ts

--- src/services/registerStudentToLab.ts
+++ src/services/registerStudentToLab.ts
@@ -2,30 +2,42 @@ import { Student } from '../student/entity';
 import { HttpError } from '../utils/HttpError';
 import { getConnection } from 'typeorm';
 import { LabClass } from '../lab-class/entity';
+import { Course } from '../course/entity';
 
 export async function registerStudentToLab(labClassID: number, studentID: number) {
   const connection = await getConnection();
   const labClassRepo = connection.getRepository(LabClass);
   const studentRepo = connection.getRepository(Student);
+  const courseRepo = connection.getRepository(Course);
 
-  const labClass = await labClassRepo.findOne(labClassID, { relations: ['course'] });
+  const labClass = await labClassRepo.findOne(labClassID, {
+    relations: ['course', 'students'],
+  });
   if (!labClass) throw new HttpError(404, 'Lab with the given id does not exist');
+
+  const course = await courseRepo.findOne(labClass.course.id, {
+    relations: ['students'],
+  });
+  if (!course) throw new HttpError(404, 'Course with the given id does not exist');
 
   const student = await studentRepo.findOne(studentID);
   if (!student) throw new HttpError(404, 'Student with the given id does not exist');
 
-  const existingRegistration = labClass.course.students.filter(
-    stud => stud.id === student.id,
-  );
+  const existingRegistration =
+    course.students && course.students.filter(stud => stud.id === student.id);
   if (existingRegistration && existingRegistration.length > 0)
     throw new HttpError(400, 'Student already registered');
 
-  if (labClass.students.length >= labClass.studentCapacity)
+  if (labClass.students && labClass.students.length >= labClass.studentCapacity)
     throw new HttpError(400, 'Lab capacity is full');
 
-  labClass.students.push(student);
-  labClass.course.students.push(student);
-  const result = await labClassRepo.save(labClass);
+  const result = await connection.transaction(async transactionalEntityManager => {
+    labClass.students.push(student);
+    course.students.push(student);
+    await transactionalEntityManager.save(course);
+    const lab = await transactionalEntityManager.save(labClass);
 
+    return lab;
+  });
   return result;
 }
