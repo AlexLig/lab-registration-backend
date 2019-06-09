e8a3a59b8adac20bedbc4d07db767d8dc5a49a9a
AlexLig <ligkalex@gmail.com>
2019-06-09 22:17:23 +0300

    Update getStudentLabs.ts

--- src/services/getStudentLabs.ts
+++ src/services/getStudentLabs.ts
@@ -4,7 +4,9 @@ import { HttpError } from '../utils/HttpError';
 
 export async function getStudentLabs(id: number) {
   const studentRepo = getRepository(Student);
-  const student = await studentRepo.findOne(id, { relations: ['labClasses'] });
+  const student = await studentRepo.findOne(id, {
+    relations: ['labClasses', 'labClasses.course'],
+  });
   if (!student) throw new HttpError(404, 'Student with the given id does not exist');
   return student.labClasses;
 }
